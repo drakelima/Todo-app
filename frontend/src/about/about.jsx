f1c81cc0f16ea62cd423e06b16500368a3739288
drakelima <drakelima@hotmail.com>
2020-03-24 11:10:10 -0300

    Projeto React-Redux

---
+++ frontend/src/about/about.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import PageHeader from '../template/pageHeader'
+
+export default props => (
+    <div>
+        <PageHeader name='Sobre' small='Nós' />
+        
+        <h2>Nossa historia</h2>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Neque accusamus enim quaerat quos optio non illo aperiam numquam molestiae et doloribus nisi est facere, velit quia? Aliquid voluptas labore possimus?</p>
+        <h2>Missão e visão</h2>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rerum explicabo velit sunt eaque obcaecati quos ab quae veniam illum fugiat sed vel nulla rem distinctio, sint maxime. Delectus, saepe autem!</p>
+        <h2>Imprensa</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam excepturi, id provident magni minima, nulla qui tempora ut nemo eum veritatis. Voluptatibus enim ut saepe nam sint dolores officia ducimus.</p>
+
+    </div>
+)
